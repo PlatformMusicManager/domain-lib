1f5286f83749128cf43e0587685abc847193eca1
unknown <david.ter.18@gmail.com>
2025-11-04 23:25:05 +0200

    await_verification pub now

--- src/cache/await_verification.rs
+++ src/cache/await_verification.rs
@@ -5,6 +5,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Serialize, Deserialize, ToRedisArgs, FromRedisValue)]
 pub struct UserAwaitVerification {
     pub email: String,
+    pub username: String,
     pub password_hash: String,
     pub code: String,
     pub active_until: DateTime<Utc>
