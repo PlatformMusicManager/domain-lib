c7d4bbfd77486d4468a07718533bde454bf548c9
Davidooff <david.ter.18@gmail.com>
2025-12-18 02:21:15 +0200

    added picture to apitrack

--- src/models/music_api/track.rs
+++ src/models/music_api/track.rs
@@ -1,6 +1,6 @@
-use serde::{Deserialize, Serialize};
 use crate::models::music_api::artist::ApiArtist;
 use crate::models::music_api::services::ApiServices;
+use serde::{Deserialize, Serialize};
 
 #[derive(Serialize)]
 pub struct ApiTrack {
@@ -11,6 +11,7 @@ pub struct ApiTrack {
     pub alb_id: Option<String>,
     pub alb_title: Option<String>,
     pub duration: i64,
+    pub picture: Option<String>,
     pub track_url: Option<String>,
     pub track_token: Option<String>,
 }
